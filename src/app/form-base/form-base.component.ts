b6ad262367be401ef797bba779d38184c2d75e04
Fernando OR <fernando.or6@gmail.com>
2024-01-09 20:14:22 -0300

    feat: create form input standalone component

--- src/app/form-base/form-base.component.ts
+++ src/app/form-base/form-base.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-base',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-base.component.html',
   styleUrl: './form-base.component.css'
 })
-export class FormBaseComponent {
+export class FormBaseComponent implements OnInit{
+
+  @Input() formFields: any[] = [];
+  form!: FormGroup;
+
+  ngOnInit(): void {
+    
+  }
+
+  onSubmit(){
+
+  }
 
 }
